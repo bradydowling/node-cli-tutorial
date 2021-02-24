57e4f2cbae8801b972739786e48b1c8cbb2aaee0
bradydowling <bradydowling@gmail.com>
2021-02-24 15:30:21 -0500

    Get main headlines

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ const getEspnHeadlines = async () => {
   const page = await browser.newPage();
   await page.goto('http://espn.com/');
   const headlines = await page.evaluate(() => {
-    const headlineItems = [...document.querySelectorAll('.headlineStack li a')];
-    return headlineItems.map(item => item.innerText);
+    const headlineItems = [...document.querySelectorAll('.col-three .headlineStack li a')];
+    return headlineItems.map(item => {
+      const postDotComText = item.href.split(".com/")[1];
+      return {
+        text: item.innerText,
+        href: item.href,
+        sport: postDotComText.split("/")[0]
+      }
+    });
   });
   await browser.close();
   return headlines;
@@ -23,7 +30,9 @@ const runCli = async () => {
   console.log("Thanks for consuming sports headlines responsibly!");
   console.log("Getting headlines...");
   const headlines = await getEspnHeadlines();
-  console.log(headlines.join("\n"));
+  headlines.forEach((headline, i) => {
+    console.log(`${i}) [${headline.sport}] ${headline.text} --> ${headline.href}`);
+  });
   const sport = await prompt.run();
   console.log(`Getting headlines for ${sport}...jk`);
 }
