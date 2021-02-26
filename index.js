c784776f21e5aa1bc385c7503033429bba78ea6b
bradydowling <bradydowling@gmail.com>
2021-02-26 14:02:57 -0500

    Show all headlines and sports as options

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const getSportsList = (document) => {
     return isEspnSite && hasSingleRoute;
   }).filter((outterItem, index, originalArray) => {
     return originalArray.findIndex(innerItem => innerItem.href === outterItem.href) === index;
-  }).sort((a, b) => a.name.localeCompare(b.name));
+  }).sort((a, b) => a.title.localeCompare(b.title));
   return sports;
 };
 
@@ -28,14 +28,16 @@ const getInitialEspnData = async () => {
       const postDotComText = item.href.split(".com/")[1];
       return {
         sport: postDotComText.split("/")[0],
-        text: item.innerText,
+        title: item.innerText,
         href: item.href,
+        type: "headline",
       };
     });
     const sports = [...document.querySelectorAll('#global-nav ul.espn-en li.sports a')].map(sport => {
       return {
-        name: sport.innerText.trim().split("\n")[0],
-        href: sport.href
+        title: sport.innerText.trim().split("\n")[0],
+        href: sport.href,
+        type: "sport",
       };
     }).filter(sport => {
       // Some filter hacks because our query for this isn't great
@@ -44,7 +46,7 @@ const getInitialEspnData = async () => {
       return isEspnSite && hasSingleRoute;
     }).filter((outterItem, index, originalArray) => {
       return originalArray.findIndex(innerItem => innerItem.href === outterItem.href) === index;
-    }).sort((a, b) => a.name.localeCompare(b.name));
+    }).sort((a, b) => a.title.localeCompare(b.title));
     return { headlines, sports }
   });
   await browser.close();
@@ -65,10 +67,10 @@ const rightPad = (string, length) => {
 };
 
 const logHeadlines = (headlines) => {
-  const maxHeadlineLength = Math.max(...headlines.map(item => item.text.length + item.sport.length));
+  const maxHeadlineLength = Math.max(...headlines.map(item => item.title.length + item.sport.length));
   // console.table(headlines);
   headlines.forEach((headline, i) => {
-    console.log(`${i + 1}) [${headline.sport}] ${rightPad(headline.text, maxHeadlineLength)} --> ${headline.href}`);
+    console.log(`${i + 1}) [${headline.sport}] ${rightPad(headline.title, maxHeadlineLength)} --> ${headline.href}`);
   });
 };
 
@@ -76,15 +78,24 @@ const runCli = async () => {
   console.log("Thanks for consuming sports headlines responsibly!");
   console.log("Getting headlines...");
   const { headlines, sports } = await getInitialEspnData();
-  logHeadlines(headlines);
-  console.table(sports);
-  // const prompt = new enquirer.Select({
-  //   name: 'color',
-  //   message: 'Which sport would you like news about?',
-  //   choices: sports.map(sport => sport.name)
-  // });
-  // const selectedSport = await prompt.run();
-  // console.log(`Getting headlines for ${selectedSport}...jk`);
+  const options = [...headlines, ...sports];
+  const choices = options.map(option => option.title);
+  const prompt = new enquirer.Select({
+    name: 'color',
+    message: 'Select a headline to get article text or a sport to see headlines for that sport',
+    choices
+  });
+  const selection = await prompt.run();
+  const selectedOption = options.find(option => option.title === selection);
+  if (selectedOption.type === "headline") {
+    console.log("This is where I'd show you the whole article");
+  }
+  else if (selectedOption.type === "sport") {
+    console.log(`This is where I'd show you headlines for ${selectedOption.title}`);
+  }
+  else {
+    console.log("Thanks for using the ESPN cli!");
+  }
 }
 
 runCli();
