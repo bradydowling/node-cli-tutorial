dafc39867d9c42ea4b6b09cf12f3ab24935e7503
bradydowling <bradydowling@gmail.com>
2021-02-27 15:26:47 -0500

    Add cheerio and axios for scraping

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 import { webkit } from 'playwright';
 import enquirer from 'enquirer';
+import axios from "axios";
+import cheerio from "cheerio";
+import * as path from "path";
+
+const homepageUrl = "http://espn.com/";
+const headlineSelector = ".col-three .headlineStack li a";
+const sportsSelector = "#global-nav ul.espn-en li.sports a";
 
 const getSportsList = (document) => {
   const sports = [...document.querySelectorAll('#global-nav ul.espn-en li.sports a')].map(sport => {
@@ -18,12 +25,44 @@ const getSportsList = (document) => {
   return sports;
 };
 
+const fetchEspnData = async () => {
+  const response = await axios.get(homepageUrl);
+  const html = response.data;
+  const $ = cheerio.load(html);
+
+  const headlines = [];
+  $(headlineSelector).each(function (i, elem) {
+    const postDotComText = $(this).attr('href').replace(/^\//, "");
+    const href = path.join(homepageUrl, postDotComText);
+    headlines[i] = {
+      title: $(this).text(),
+      sport: postDotComText.split("/")[0],
+      href,
+      type: "headline",
+    }
+  });
+
+  const sports = [];
+  $(sportsSelector).each(function (i, elem) {
+    const postDotComText = $(this).attr('href').replace(/^\//, "");
+    const href = path.join(homepageUrl, postDotComText);
+    sports[i] = {
+      title: $(this).text().trim().split("\n")[0].toLowerCase(),
+      href,
+      type: "sport",
+    }
+  });
+
+  return { headlines, sports };
+}
+
 const getInitialEspnData = async () => {
   const browser = await webkit.launch();
   const page = await browser.newPage();
-  await page.goto('http://espn.com/');
+  await page.goto(homepageUrl);
   const { headlines, sports } = await page.evaluate(() => {
-    const headlineItems = [...document.querySelectorAll('.col-three .headlineStack li a')];
+    const headlineSelector = ".col-three .headlineStack li a";
+    const headlineItems = [...document.querySelectorAll(headlineSelector)];
     const headlines = headlineItems.map(item => {
       const postDotComText = item.href.split(".com/")[1];
       return {
@@ -33,7 +72,8 @@ const getInitialEspnData = async () => {
         type: "headline",
       };
     });
-    const sports = [...document.querySelectorAll('#global-nav ul.espn-en li.sports a')].map(sport => {
+    const sportsSelector = "#global-nav ul.espn-en li.sports a";
+    const sports = [...document.querySelectorAll(sportsSelector)].map(sport => {
       return {
         title: sport.innerText.trim().split("\n")[0],
         href: sport.href,
@@ -53,31 +93,15 @@ const getInitialEspnData = async () => {
   return { headlines, sports };
 };
 
-// const rightPad = (string, length) => {
-//   const newString = string.slice();
-//   while (newString.length < length) {
-//     newString.push(" ");
-//   }
-//   return newString;
-// }
-
 const rightPad = (string, length) => {
   if(length <= string.length) return string;
   return string + new Array(length - string.length + 1).join(" ");
 };
 
-const logHeadlines = (headlines) => {
-  const maxHeadlineLength = Math.max(...headlines.map(item => item.title.length + item.sport.length));
-  // console.table(headlines);
-  headlines.forEach((headline, i) => {
-    console.log(`${i + 1}) [${headline.sport}] ${rightPad(headline.title, maxHeadlineLength)} --> ${headline.href}`);
-  });
-};
-
 const runCli = async () => {
   console.log("Thanks for consuming sports headlines responsibly!");
   console.log("Getting headlines...");
-  const { headlines, sports } = await getInitialEspnData();
+  const { headlines, sports } = await fetchEspnData();
   const options = [...headlines, ...sports];
   const choices = options.map(option => option.title);
   const prompt = new enquirer.Select({
